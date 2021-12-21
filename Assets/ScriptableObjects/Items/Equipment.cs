bf66f1c136969a64f71a7fb40b23c38fc973fdec
ByteofSin <Byteofsin@gmail.com>
2021-12-21 12:13:47 -0600

    Added basic functionality to items
    still doesnt do much but it is a template

--- Assets/ScriptableObjects/Items/Equipment.cs
+++ Assets/ScriptableObjects/Items/Equipment.cs
@@ -1,18 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+namespace Byte.Item {
+    public class Equipment : Item {
 
-public class Equipment : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/ScriptableObjects/Items/Item.cs
+++ Assets/ScriptableObjects/Items/Item.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+namespace Byte.Item {
+    public class Item : ScriptableObject {
+        protected new string name;
+        protected string description;
+
+        public string GetName(){
+            return name;
+        }
+
+        public string GetDescription(){
+            return description;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
